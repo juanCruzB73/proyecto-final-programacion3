cc833b72200f1f82eb62549e361f9f8d92efcbb5
juanB <juancruzberrios73@gmail.com>
2024-11-05 04:09:00 -0300

    entrega 2  terminada - falta pulir css y subcategorias e image upload en administracion

--- src/components/categoriasAlergenos/Card.tsx
+++ src/components/categoriasAlergenos/Card.tsx
@@ -19,6 +19,7 @@ interface Iprops{
 export const Card:FC<Iprops> = ({element}) => {
   
   const {categoriaFilter,productoFilter,alergenosFilter,}=useSelector((state:RootState)=>state.administracion)
+  const {elementActiveCategoria,administracionTable}=useSelector((state:RootState)=>state.tableAdministracion)
   const categoriaService=new CategoriasService('http://190.221.207.224:8090/categorias');
   const articuloService=new ProductoService('http://190.221.207.224:8090/articulos');
   const alergenosService=new AlergenosService('http://190.221.207.224:8090/alergenos');
@@ -61,62 +62,81 @@ export const Card:FC<Iprops> = ({element}) => {
 
   return (
     <>
-    {
-        categoriaFilter&&(
-          <>
-            <span className='categoria'>{element.denominacion?element.denominacion : "no name"}
-              <div> 
-                <i onClick={()=>setDisplay(!display)} className="bi bi-arrow-down-circle"></i> <i  onClick={()=>{
-                  category&&dispatch(setElementActiveAdministracionCategoria({element:category}))
-                  dispatch(onEditCategoria())
-                  }} className="bi bi-pencil-square"></i> <i onClick={()=>dispatch(onAddSubCategoria())} className="bi bi-plus-circle"></i>
-              </div></span> 
-            <div className={ display ? 'subcategorias' : "notDisplayed"}>
-              { ("subCategorias" in element)? element.subCategorias.map(subCategoria=>(
-                <span className='categoria' key={subCategoria.id}>{subCategoria.denominacion?subCategoria.denominacion : "no name"}
-                <div> 
-                 <i  onClick={()=>{
-                    dispatch(onEditSubCategoria())
-                    
-                    }} className="bi bi-pencil-square"></i>
-                </div></span>
-              )): <span>no hay subcategorias</span> }
+      {categoriaFilter && (
+        <>
+          <span className="categoria">
+            {element.denominacion ? element.denominacion : 'no name'}
+            <div>
+              <i onClick={() => setDisplay(!display)} className="bi bi-arrow-down-circle"></i>
+              <i
+                onClick={() => {
+                  category && dispatch(setElementActiveAdministracionCategoria({ element: category }));
+                  dispatch(onEditCategoria());
+                }}
+                className="bi bi-pencil-square"
+              ></i>
+              <i onClick={() => dispatch(onAddSubCategoria())} className="bi bi-plus-circle"></i>
             </div>
-          </>
-        )
-      }
-      {
-        productoFilter&&(
-          <>
-            <span className='categoria'>{element.denominacion?element.denominacion : "no name"} <div>
-              <i className="bi bi-eye-fill"></i> <i onClick={()=>{
-                product && dispatch(setElementActiveAdministracionProductos({element:product}))
-                dispatch(onEditProducto())}
-                } className="bi bi-pencil-square"></i> <i className="bi bi-trash"></i></div></span>
-            <div className={ display ? 'subcategorias' : "notDisplayed"}>
-              <span className='categoria'>item</span>
-              <span className='categoria'>item</span>
+          </span>
+          <div className={display ? 'subcategorias' : 'notDisplayed'}>
+            {administracionTable
+              ? administracionTable.map(subCategoria => (
+                  <span className="categoria" key={subCategoria.id}>
+                    {subCategoria.denominacion ? subCategoria.denominacion : 'no name'}
+                    <div>
+                      <i onClick={() => dispatch(onEditSubCategoria())} className="bi bi-pencil-square"></i>
+                    </div>
+                  </span>
+                ))
+              : <span>no hay subcategorias</span>}
+          </div>
+        </>
+      )}
+      {productoFilter && (
+        <>
+          <span className="categoria">
+            {element.denominacion ? element.denominacion : 'no name'}
+            <div>
+              <i className="bi bi-eye-fill"></i>
+              <i
+                onClick={() => {
+                  product && dispatch(setElementActiveAdministracionProductos({ element: product }));
+                  dispatch(onEditProducto());
+                }}
+                className="bi bi-pencil-square"
+              ></i>
+              <i className="bi bi-trash"></i>
             </div>
-          </>
-          )
-      }
-      {
-        alergenosFilter&&(
-          <>
-            <span className='categoria'>{element.denominacion?element.denominacion : "no name"} <div>
-              <i className="bi bi-eye-fill"></i> <i onClick={()=>{{
-                alergeno&&dispatch(setElementActiveAdministracionAlergenos({element:alergeno}))
-                dispatch(onEditAlergeno())
-                }}} className="bi bi-pencil-square"></i> <i className="bi bi-trash"></i></div></span>
-            <div className={ display ? 'subcategorias' : "notDisplayed"}>
-              <span className='categoria'>item</span>
-              <span className='categoria'>item</span>
+          </span>
+          <div className={display ? 'subcategorias' : 'notDisplayed'}>
+            <span className="categoria">item</span>
+            <span className="categoria">item</span>
+          </div>
+        </>
+      )}
+      {alergenosFilter && (
+        <>
+          <span className="categoria">
+            {element.denominacion ? element.denominacion : 'no name'}
+            <div>
+              <i className="bi bi-eye-fill"></i>
+              <i
+                onClick={() => {
+                  alergeno && dispatch(setElementActiveAdministracionAlergenos({ element: alergeno }));
+                  dispatch(onEditAlergeno());
+                }}
+                className="bi bi-pencil-square"
+              ></i>
+              <i className="bi bi-trash"></i>
             </div>
-          </>
-        )
-      }
-        
+          </span>
+          <div className={display ? 'subcategorias' : 'notDisplayed'}>
+            <span className="categoria">item</span>
+            <span className="categoria">item</span>
+          </div>
+        </>
+      )}
     </>
-  )
+  );
 }
 
