c642e6f8eff3cd882bea0c9532f633b7f2e1413b
Paula Geier <geierpaulasofia@gmail.com>
2024-11-05 21:31:54 -0300

    Card ver alergeno y producto

---
+++ src/views/ProductosDetails/ProductosDetail.tsx
@@ -0,0 +1,20 @@
+import { Card } from "react-bootstrap"
+import "./ProductosDetails.css"
+
+const ProductosDetail = () => {
+    return (
+        <div className="detailsContainer">
+            <Card className="cardContainer" style={{ width: '18rem' }}>
+                <div className="imagenContainer">
+                <Card.Img className="imagen" variant="top" src="holder.js/100px180" />
+                </div>
+
+                <Card.Body className="text">
+                    <Card.Title>Denominación:</Card.Title>
+                </Card.Body>
+            </Card>
+        </div>
+    )
+}
+
+export default ProductosDetail
